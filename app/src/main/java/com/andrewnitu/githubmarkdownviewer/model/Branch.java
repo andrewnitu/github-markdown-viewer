0535dd6dae82052428784578d5ab02dd9bc50a10
andrewn <andrewnitu@gmail.com>
2017-05-29 23:41:23 -0400

    Add data class to represent GitHub branch info

---
+++ app/src/main/java/com/andrewnitu/githubmarkdownviewer/model/Branch.java
@@ -0,0 +1,17 @@
+package com.andrewnitu.githubmarkdownviewer.model;
+
+/**
+ * Created by Andrew Nitu on 5/29/2017.
+ */
+
+public class Branch {
+    String name;
+
+    public Branch (String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
