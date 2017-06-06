c7931b1dea624eb8ee65733737e8141f53ccd7ef
andrewn <andrewnitu@gmail.com>
2017-06-06 01:06:37 -0400

    Add object to represent a file within a repo on GitHub

---
+++ app/src/main/java/com/andrewnitu/githubmarkdownviewer/model/File.java
@@ -0,0 +1,23 @@
+package com.andrewnitu.githubmarkdownviewer.model;
+
+/**
+ * Created by Andrew Nitu on 6/5/2017.
+ */
+
+public class File {
+    String name;
+    String path;
+
+    public File (String name, String path) {
+        this.name = name;
+        this.path = path;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getPath(){
+        return name;
+    }
+}
