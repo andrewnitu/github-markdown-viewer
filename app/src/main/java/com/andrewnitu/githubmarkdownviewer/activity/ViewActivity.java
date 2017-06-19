ebeda1ecba5e87e38e86662a7404d68d3de5974f
andrewn <andrewnitu@gmail.com>
2017-06-19 01:41:50 -0400

    Get HTML contents from Markdown file properly using StringRequest, attaching media type header, and passing branchname param

--- app/src/main/java/com/andrewnitu/githubmarkdownviewer/activity/ViewActivity.java
+++ app/src/main/java/com/andrewnitu/githubmarkdownviewer/activity/ViewActivity.java
@@ -27,52 +27,65 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class ViewActivity extends AppCompatActivity implements TouchListener {
     final String baseUrl = "https://api.github.com";
+    String username;
+    String reponame;
+    String filepath;
+    String branchname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_view);
+
+        Intent intent = getIntent();
+        username = intent.getStringExtra("Username");
+        reponame = intent.getStringExtra("Reponame");
+        filepath = intent.getStringExtra("Filepath");
+        branchname = intent.getStringExtra("Branchname");
+
+        fileRequest(username, reponame, filepath, branchname);
     }
 
-    public void fileRequest(final String reqUsername, final String reqReponame) {
+    public void fileRequest(final String reqUsername, final String reqReponame, final String reqFilepath, final String reqBranchname) {
         // Instantiate the RequestQueue
         RequestQueue queue = Volley.newRequestQueue(this);
 
         // Create the URL to request the specific file
-        String requestURL = baseUrl + "/users/" + reqUsername + "/repos";
+        final String requestURL = baseUrl + "/repos/" + reqUsername + "/" + reqReponame + "/contents/" + reqFilepath + "?ref=" + reqBranchname;
 
         // Request a string response from the provided URL
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, requestURL,
-                new Response.Listener<JSONArray>() {
+        StringRequest stringRequest = new StringRequest(requestURL,
+                new Response.Listener<String>() {
                     // Do on a successful request
                     @Override
-                    public void onResponse(JSONArray response) {
-                        Log.e(response.toString());
-                        //try {
-                        //                      } catch (JSONException e) {
-                        //                    }
+                    public void onResponse(String response) {
+                        Log.e("SUCCESS", response);
                     }
                 },
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         // Give an error!
+                        Log.e("FAILURE", "At URL: " + requestURL);
+
                         Toast toast = Toast.makeText(getApplicationContext(), "Couldn't obtain that file!", Toast.LENGTH_LONG);
                         toast.show();
                     }
-                }) {
+                }
+        ) {
             @Override
             public Map<String, String> getHeaders() throws AuthFailureError {
                 Map<String, String> params = new HashMap<String, String>();
@@ -83,7 +96,7 @@ public class ViewActivity extends AppCompatActivity implements TouchListener {
         };
 
         // Add the request to the RequestQueue
-        queue.add(jsonArrayRequest);
+        queue.add(stringRequest);
     }
 
     @Override
