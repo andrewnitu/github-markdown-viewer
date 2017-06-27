343d3db83fbd79dfc22cbbb67225c8b50bada7fc
andrewn <andrewnitu@gmail.com>
2017-06-27 01:10:32 -0400

    Convert this activity into a simple container with a toolbar and the settings fragment

--- app/src/main/java/com/andrewnitu/githubmarkdownviewer/activity/SettingsActivity.java
+++ app/src/main/java/com/andrewnitu/githubmarkdownviewer/activity/SettingsActivity.java
@@ -1,8 +1,21 @@
 package com.andrewnitu.githubmarkdownviewer.activity;
 
-/**
- * Created by Andrew Nitu on 6/22/2017.
- */
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-public class SettingsActivity {
+import com.andrewnitu.githubmarkdownviewer.R;
+import com.andrewnitu.githubmarkdownviewer.fragment.SettingsFragment;
+
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.settings_toolbar);
+        setSupportActionBar(toolbar);
+
+        getFragmentManager().beginTransaction().replace(R.id.content_frame, new SettingsFragment()).commit();
+    }
 }
