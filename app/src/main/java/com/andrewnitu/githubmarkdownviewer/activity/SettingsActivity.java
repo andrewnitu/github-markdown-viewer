99cd067b352622001b3ac0de9a16f4c561f59ae1
andrewn <andrewnitu@gmail.com>
2017-06-28 00:59:08 -0400

    Add back button to settings toolbar, works similar to Google Play Store settings

--- app/src/main/java/com/andrewnitu/githubmarkdownviewer/activity/SettingsActivity.java
+++ app/src/main/java/com/andrewnitu/githubmarkdownviewer/activity/SettingsActivity.java
@@ -3,6 +3,7 @@ package com.andrewnitu.githubmarkdownviewer.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.andrewnitu.githubmarkdownviewer.R;
 import com.andrewnitu.githubmarkdownviewer.fragment.SettingsFragment;
@@ -16,6 +17,19 @@ public class SettingsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.settings_toolbar);
         setSupportActionBar(toolbar);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
         getFragmentManager().beginTransaction().replace(R.id.content_frame, new SettingsFragment()).commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
