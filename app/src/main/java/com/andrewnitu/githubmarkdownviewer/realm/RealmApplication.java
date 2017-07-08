e41bfa3668d515035e1deb1eaa90317dd09de50d
andrewn <andrewnitu@gmail.com>
2017-07-08 01:46:04 -0400

    Fix issue caused by reading from outdated docs

--- app/src/main/java/com/andrewnitu/githubmarkdownviewer/realm/RealmApplication.java
+++ app/src/main/java/com/andrewnitu/githubmarkdownviewer/realm/RealmApplication.java
@@ -1,6 +1,7 @@
 package com.andrewnitu.githubmarkdownviewer.realm;
 
 import android.app.Application;
+import android.util.Log;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
@@ -9,8 +10,10 @@ public class RealmApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Realm.init(this);
         RealmConfiguration config =
-                new RealmConfiguration.Builder().name("GHMDViewerFavourites").build();
+                new RealmConfiguration.Builder().name("ghmdviewerfavos.realm").build();
         Realm.setDefaultConfiguration(config);
+        Log.d("Realm setup", "Realm initialized and configured");
     }
 }
