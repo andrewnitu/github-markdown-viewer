9bf3a901e4cc736a66a259bfb9e4b03de20db5fe
andrewn <andrewnitu@gmail.com>
2017-07-13 00:16:16 -0400

    Add favourites support to FileListAdapter, favourites for files is now working

--- app/src/main/java/com/andrewnitu/githubmarkdownviewer/adapter/FileListAdapter.java
+++ app/src/main/java/com/andrewnitu/githubmarkdownviewer/adapter/FileListAdapter.java
@@ -1,6 +1,5 @@
 package com.andrewnitu.githubmarkdownviewer.adapter;
 
-import android.media.Image;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,9 +8,12 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.andrewnitu.githubmarkdownviewer.R;
+import com.andrewnitu.githubmarkdownviewer.model.db.RealmFile;
 import com.andrewnitu.githubmarkdownviewer.model.local.File;
 
 import java.util.List;
+
+import io.realm.Realm;
 
 /**
  * Created by Andrew Nitu on 5/15/2017.
@@ -20,10 +22,14 @@ import java.util.List;
 public class FileListAdapter extends RecyclerView.Adapter<FileListAdapter.BranchViewHolder> {
     private List<File> files;
 
+    private Realm realmInstance;
+
     private ClickListener clickListener = null;
 
     public FileListAdapter(List<File> files){
         this.files = files;
+
+        realmInstance = Realm.getDefaultInstance();
     }
 
     @Override
@@ -40,22 +46,30 @@ public class FileListAdapter extends RecyclerView.Adapter<FileListAdapter.Branch
     @Override
     public void onBindViewHolder(BranchViewHolder bvh, int i) {
         bvh.branchTitle.setText(files.get(i).getName());
+
+        if (realmInstance.where(RealmFile.class).equalTo("name", files.get(i).getName()).equalTo("url", files.get(i).getUrl()).findFirst() != null) {
+            bvh.branchFavouriteIcon.setImageResource(R.drawable.ic_star_filled);
+        }
     }
 
     public class BranchViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        protected TextView branchTitle;
-        protected ImageView repoFavouriteIcon;
+        private TextView branchTitle;
+        private ImageView branchFavouriteIcon;
 
-        public BranchViewHolder(View itemView) {
+        private BranchViewHolder(View itemView) {
             super(itemView);
+
+            branchTitle = (TextView) itemView.findViewById(R.id.file_title);
+            branchFavouriteIcon = (ImageView) itemView.findViewById(R.id.favourite_icon);
+
+            branchFavouriteIcon.setOnClickListener(this);
             itemView.setOnClickListener(this);
-            branchTitle = (TextView)itemView.findViewById(R.id.branch_title);
         }
 
         @Override
         public void onClick(View v) {
             if (clickListener != null) {
-                if (v.getId() == repoFavouriteIcon.getId()) {
+                if (v.getId() == branchFavouriteIcon.getId()) {
                     clickListener.onFavouriteClicked(v, getAdapterPosition());
                 }
                 else {
