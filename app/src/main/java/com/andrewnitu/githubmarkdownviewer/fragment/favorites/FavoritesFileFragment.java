39d50690c0c66b3ea3ad9d974b2e8cd439904331
andrewn <andrewnitu@gmail.com>
2017-07-19 01:29:30 -0400

    Add fragments for both favorites tabs

---
+++ app/src/main/java/com/andrewnitu/githubmarkdownviewer/fragment/favorites/FavoritesFileFragment.java
@@ -0,0 +1,30 @@
+package com.andrewnitu.githubmarkdownviewer.fragment.favourites;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.andrewnitu.githubmarkdownviewer.R;
+
+public class FavoritesFileFragment extends Fragment {
+    View rootView;
+    ActionBar savedActivityToolbar;
+
+    public FavoritesFileFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        rootView = inflater.inflate(R.layout.fragment_repo_favorites, container, false);
+
+        return rootView;
+    }
+
+}
