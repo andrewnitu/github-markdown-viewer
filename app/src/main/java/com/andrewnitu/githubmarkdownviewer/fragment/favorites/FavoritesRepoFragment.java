7b276c2062e6618b09c02627460a0078bc2f1a70
andrewn <andrewnitu@gmail.com>
2017-07-20 01:41:05 -0400

    Add basic favorites list logic

--- app/src/main/java/com/andrewnitu/githubmarkdownviewer/fragment/favorites/FavoritesRepoFragment.java
+++ app/src/main/java/com/andrewnitu/githubmarkdownviewer/fragment/favorites/FavoritesRepoFragment.java
@@ -1,20 +1,47 @@
 package com.andrewnitu.githubmarkdownviewer.fragment.favorites;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.andrewnitu.githubmarkdownviewer.R;
+import com.andrewnitu.githubmarkdownviewer.activity.RepoActivity;
+import com.andrewnitu.githubmarkdownviewer.adapter.ClickListener;
+import com.andrewnitu.githubmarkdownviewer.adapter.RepoListAdapter;
+import com.andrewnitu.githubmarkdownviewer.model.db.RealmRepo;
+import com.andrewnitu.githubmarkdownviewer.model.local.Repo;
 
-public class FavoritesRepoFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.UUID;
+
+import io.realm.Realm;
+import io.realm.RealmQuery;
+import io.realm.RealmResults;
+
+public class FavoritesRepoFragment extends Fragment implements ClickListener {
     View rootView;
     ActionBar savedActivityToolbar;
 
-    public FavoritesRepoFragment() {
-        // Required empty public constructor
+    private Realm realmInstance;
+
+    private RecyclerView recyclerView;
+    private ArrayList<Repo> repos;
+    private RepoListAdapter adapter;
+
+    @Override
+    public void onResume() {  // After a pause OR at startup
+        super.onResume();
+
+        adapter.notifyDataSetChanged();
     }
 
     @Override
@@ -24,7 +51,86 @@ public class FavoritesRepoFragment extends Fragment {
         setRetainInstance(true);
         rootView = inflater.inflate(R.layout.fragment_repo_favorites, container, false);
 
+        // Get our Realm instance
+        realmInstance = Realm.getDefaultInstance();
+
+        RealmQuery<RealmRepo> allRepoQuery = realmInstance.where(RealmRepo.class);
+
+        RealmResults<RealmRepo> allRepoResults = allRepoQuery.findAll();
+
+        repos = new ArrayList<Repo>();
+
+        for (int i = 0; i < allRepoResults.size(); i++) {
+            repos.add(new Repo(allRepoResults.get(i).getName(), allRepoResults.get(i).getOwnerUserName()));
+        }
+
+        // Bind our UI elements
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this.getContext());
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(llm);
+
+        // Set up RecyclerView row dividers
+        DividerItemDecoration mDividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+                llm.getOrientation());
+        recyclerView.addItemDecoration(mDividerItemDecoration);
+
+        // Give our RecyclerView an adapter
+        adapter = new RepoListAdapter(repos);
+        recyclerView.setAdapter(adapter);
+
+        adapter.setClickListener(this);
+
         return rootView;
     }
 
+    @Override
+    public void onRowClicked(View view, int index) {
+        Intent intent = new Intent(getActivity(), RepoActivity.class);
+        intent.putExtra("Username", repos.get(index).getOwnerUserName());
+        intent.putExtra("Reponame", repos.get(index).getName());
+
+        startActivity(intent);
+    }
+
+    @Override
+    public void onFavouriteClicked(View view, int index) {
+        RealmQuery<RealmRepo> repoQuery = realmInstance.where(RealmRepo.class).equalTo("name", repos.get(index).getName()).equalTo("ownerUserName", repos.get(index).getOwnerUserName());
+
+        RealmResults<RealmRepo> repoResults = repoQuery.findAll();
+
+        int numResults = repoResults.size();
+
+        realmInstance.beginTransaction();
+        if (numResults == 0) {
+            Log.d("Realm Transaction", "Added Repo object");
+            RealmRepo repo = realmInstance.createObject(RealmRepo.class, UUID.randomUUID().toString());
+            repo.setName(repos.get(index).getName());
+            repo.setOwnerUserName(repos.get(index).getOwnerUserName());
+            switchFavouritesIcon(true, view);
+        } else {
+            Log.d("Realm Transaction", "Removed Repo object");
+            repoResults.first().deleteFromRealm();
+            switchFavouritesIcon(false, view);
+        }
+        realmInstance.commitTransaction();
+    }
+
+    // Switches the favourites icon in the view
+    // Takes a state (true for favourited, false for unfavourited) and the View of the row clicked
+    public void switchFavouritesIcon(boolean state, View view) {
+        ImageView icon = (ImageView) view.findViewById(R.id.favourite_icon);
+        if (state) {
+            icon.setImageResource(R.drawable.ic_star_filled);
+        } else {
+            icon.setImageResource(R.drawable.ic_star_empty);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        realmInstance.close();
+    }
 }
