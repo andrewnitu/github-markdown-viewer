1c4948eb8049dd88d6212cbf27e2c97cc9e74a70
andrewn <andrewnitu@gmail.com>
2017-07-25 00:13:33 -0400

    Create list adapters with different bindings for the different types of list items in the favorites screen

---
+++ app/src/main/java/com/andrewnitu/githubmarkdownviewer/adapter/favorites/FileListFavoritesAdapter.java
@@ -0,0 +1,91 @@
+package com.andrewnitu.githubmarkdownviewer.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.andrewnitu.githubmarkdownviewer.R;
+import com.andrewnitu.githubmarkdownviewer.model.db.RealmFile;
+import com.andrewnitu.githubmarkdownviewer.model.local.File;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+import io.realm.Realm;
+
+/**
+ * Created by Andrew Nitu on 5/15/2017.
+ */
+
+public class FileListFavoritesAdapter extends RecyclerView.Adapter<FileListFavoritesAdapter.FileViewHolder> {
+    private List<File> files;
+
+    private Realm realmInstance;
+
+    private ClickListener clickListener = null;
+
+    public FileListFavoritesAdapter(List<File> files){
+        this.files = files;
+
+        realmInstance = Realm.getDefaultInstance();
+    }
+
+    @Override
+    public int getItemCount() {
+        return files.size();
+    }
+
+    @Override
+    public FileViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item_file_favorites, viewGroup, false);
+        return new FileViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(FileViewHolder fvh, int i) {
+        fvh.fileTitleAndBranch.setText(files.get(i).getPath() + " - " + files.get(i).getBranchName());
+        fvh.fileRepoAndOwnerUserName.setText(files.get(i).getRepoName() + " - " + files.get(i).getOwnerUserName());
+
+        if (realmInstance.where(RealmFile.class).equalTo("ownerUserName", files.get(i).getOwnerUserName())
+                .equalTo("repoName", files.get(i).getRepoName()).equalTo("path", files.get(i).getPath()).equalTo("branchName", files.get(i).getBranchName()).findFirst() != null) {
+            fvh.fileFavouriteIcon.setImageResource(R.drawable.ic_star_filled);
+        }
+    }
+
+    public class FileViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private TextView fileTitleAndBranch;
+        private TextView fileRepoAndOwnerUserName;
+        private ImageView fileFavouriteIcon;
+
+        private FileViewHolder(View itemView) {
+            super(itemView);
+
+            fileTitleAndBranch = (TextView) itemView.findViewById(R.id.file_title_and_branch);
+            fileRepoAndOwnerUserName = (TextView) itemView.findViewById(R.id.file_repo_and_owner_user_name);
+            fileFavouriteIcon = (ImageView) itemView.findViewById(R.id.favourite_icon);
+
+            fileFavouriteIcon.setOnClickListener(this);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (clickListener != null) {
+                if (v.getId() == fileFavouriteIcon.getId()) {
+                    clickListener.onFavouriteClicked(v, getAdapterPosition());
+                }
+                else {
+                    clickListener.onRowClicked(v, getAdapterPosition());
+                }
+            }
+        }
+    }
+
+    public void setClickListener(ClickListener clickListener) {
+        this.clickListener = clickListener;
+    }
+}
---
+++ app/src/main/java/com/andrewnitu/githubmarkdownviewer/adapter/favorites/RepoListFavoritesAdapter.java
@@ -0,0 +1,89 @@
+package com.andrewnitu.githubmarkdownviewer.adapter.favorites;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.andrewnitu.githubmarkdownviewer.R;
+import com.andrewnitu.githubmarkdownviewer.adapter.ClickListener;
+import com.andrewnitu.githubmarkdownviewer.model.db.RealmRepo;
+import com.andrewnitu.githubmarkdownviewer.model.local.Repo;
+
+import java.util.List;
+
+import io.realm.Realm;
+
+/**
+ * Created by Andrew Nitu on 5/15/2017.
+ */
+
+public class RepoListFavoritesAdapter extends RecyclerView.Adapter<RepoListFavoritesAdapter.RepoViewHolder> {
+    private List<Repo> repos;
+
+    private Realm realmInstance;
+
+    private ClickListener clickListener = null;
+
+    public RepoListFavoritesAdapter(List<Repo> repos){
+        this.repos = repos;
+
+        realmInstance = Realm.getDefaultInstance();
+    }
+
+    @Override
+    public int getItemCount() {
+        return repos.size();
+    }
+
+    @Override
+    public RepoViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item_repo_favorites, viewGroup, false);
+        return new RepoViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(final RepoViewHolder rvh, int i) {
+        rvh.repoTitle.setText(repos.get(i).getName());
+        rvh.repoOwnerUserName.setText(repos.get(i).getOwnerUserName());
+
+        if (realmInstance.where(RealmRepo.class).equalTo("name", repos.get(i).getName()).equalTo("ownerUserName", repos.get(i).getOwnerUserName()).findFirst() != null) {
+            rvh.repoFavouriteIcon.setImageResource(R.drawable.ic_star_filled);
+        }
+    }
+
+    public class RepoViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private TextView repoTitle;
+        private TextView repoOwnerUserName;
+        private ImageView repoFavouriteIcon;
+
+        private RepoViewHolder(View itemView) {
+            super(itemView);
+
+            repoTitle = (TextView) itemView.findViewById(R.id.repo_title);
+            repoOwnerUserName = (TextView) itemView.findViewById(R.id.repo_owner_user_name);
+            repoFavouriteIcon = (ImageView) itemView.findViewById(R.id.favourite_icon);
+
+            itemView.setOnClickListener(this);
+            repoFavouriteIcon.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (clickListener != null) {
+                if (v.getId() == repoFavouriteIcon.getId()) {
+                    clickListener.onFavouriteClicked(v, getAdapterPosition());
+                }
+                else {
+                    clickListener.onRowClicked(v, getAdapterPosition());
+                }
+            }
+        }
+    }
+
+    public void setClickListener(ClickListener clickListener) {
+        this.clickListener = clickListener;
+    }
+}
